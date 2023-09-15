f9815dee48919f3d09d5d519d1c16e2088e41b17
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-15 14:07:48 -0500

    Added utils and coloring

--- stem/stem1/__init__.py
+++ stem/stem1/__init__.py
@@ -1,1 +1,4 @@
-
+from stem1 import constants
+from stem1 import engine
+from stem1 import real
+from stem1 import utils
---
+++ stem/stem1/__main__.py
@@ -0,0 +1,7 @@
+print("Loading STEM1 ...")
+
+from stem1 import *
+
+colors = utils.Colors()
+
+print(colors.fg.lime, "Space-Time Engineering Miniature v1 loaded successfully.", colors.reset())
