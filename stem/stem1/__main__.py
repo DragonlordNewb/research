427e50667a39af9cffba59866bfa2cf99372f2b9
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-15 14:08:11 -0500

    Fixed minor err

--- stem/stem1/__main__.py
+++ stem/stem1/__main__.py
@@ -4,4 +4,4 @@ from stem1 import *
 
 colors = utils.Colors()
 
-print(colors.fg.lime, "Space-Time Engineering Miniature v1 loaded successfully.", colors.reset())
+print(colors.fg.lime, "Space-Time Engineering Miniature v1 loaded successfully.", colors.reset)
