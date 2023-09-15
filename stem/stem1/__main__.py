6ec59b1ef2d684c2dce64e4e32ee429f4878567e
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-15 14:10:24 -0500

    Gonna try out commands

--- stem/stem1/__main__.py
+++ stem/stem1/__main__.py
@@ -4,4 +4,11 @@ from stem1 import *
 
 colors = utils.Colors()
 
-print(colors.fg.lime, "Space-Time Engineering Miniature v1 loaded successfully.", colors.reset)
+print(colors.fg.lime + "Space-Time Engineering Miniature v1 loaded successfully." + colors.reset)
+print("Enter any command to continue.")
+
+while True:
+	usr = input(" > ")
+	match usr.split(" "):
+		case ("exit", *_):
+			print(colors.fg.red + "Exiting ..." + colors.reset)
