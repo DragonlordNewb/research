17d29d98482e20d3640b2c9b2f114b887529d187
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-20 14:36:25 -0500

    Worked more on ASTER CLI

--- aster/__main__.py
+++ aster/__main__.py
@@ -1,8 +1,12 @@
+from typing import Callable
+
 from aster.utils import ColorPrinter
+
+__version__ = "0.0.1"
 
 cpt = ColorPrinter() # Color printer!
 
-cpt("Loading ...", cpt.fg.yellow)
+cpt("Loading ...", cpt.fg.lime)
 
 cpt("  Loading aster.constants ...", cpt.fg.yellow, end="")
 from aster import constants
@@ -17,3 +21,23 @@ from aster import utils
 cpt("\r  aster.utils successfully loaded.", cpt.fg.lime)
 
 cpt("Advanced SpaceTime Engineering Research software loaded successfully!", cpt.fg.lime)
+cpt("Command line interface loading now ... Type any command, or \"help\" for a list of commands.")
+
+while True:
+	try:
+
+		cmd = input(" > ").split(" ")
+
+		match cmd:
+			case ("exit", *_):
+				cpt("Exiting ...", cpt.fg.red)
+				exit()
+
+			case ("sys", "version"):
+				cpt("This is Advanced SpaceTime Engineering Research software version " + __version__ + ".")
+
+			case *:
+				cpt("Invalid command", cpt.fg.red)
+
+	except KeyboardInterrupt:
+		continue
