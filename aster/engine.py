726fd654e38b94c11e77210bc09adfe3d6b5c360
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-20 08:27:52 -0500

    Continued work on engine, prepare to copy stuff over from other module

--- aster/engine.py
+++ aster/engine.py
@@ -1,6 +1,26 @@
 from abc import ABC
 from abc import abstractmethod
 
+class Body(ABC):
+	"""
+ 	A Python representation of a physical object.
+	"""
+
+	@abstractmethod
+	def points(self) -> None:
+		pass
+
+class Metric(ABC):
+	"""
+ 	A Python representation of a spacetime metric.
+  	Must adequately be subclassed to describe how an
+   	object experiences time dilation, space contraction,
+    	etc.
+     	"""
+
+	@abstractmethod
+	def timeDilation(self, object: 
+
 class Spacetime:
 	"""
  	A Python representation of spacetime.
@@ -10,3 +30,5 @@ class Spacetime:
  	will govern the behavior of the spacetime's geometry.
 	"""
 
+	def __init__(self, metric: Metric) -> None:
+		
