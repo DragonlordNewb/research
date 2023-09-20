28bfdafd4453c994581f07c9b1bb090a9cffe12b
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-20 08:21:26 -0500

    Imported constants from STEM1

---
+++ aster/constants.py
@@ -0,0 +1,10 @@
+F = 96485.3321 # Faraday constant
+NA = 6.02214076e+23 # Avogadro's number
+h = 6.62607015e-34 # Planck constant
+Qe = F / NA # Elementary charge
+alpha = 7.2973525693e-3 # Fine-structure constant
+c = 299792458 # Speed of light in a vacuum
+c2 = c ** 2 # c^2
+G = 6.6743e-11 # Gravitational constant
+mu0 = (2 * alpha * h) / ((Qe ** 2) * c) # Vacuum permeability
+epsilon0 = 1 / (mu0 * c2) # Vacuum permittivity
