1264c709a5daa34dc21ea3782b8543ced0fbff82
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-21 13:40:14 -0500

    Tried to add in 3-vectors

--- partibox.cpp
+++ partibox.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+class Vector {
+	private:
+		float x;
+		float y;
+		float z;
+	public:
+		Vector(float _x, float _y, float _z) {
+			x = _x;
+			y = _y;
+			z = _z;
+		}
+
+		Vector operator+(Vector const& obj) {
+			xn = x + obj.x;
+			yn = y + obj.y;
+			zn = z + obj.z; 
+			return Vector(xn, yn, zn);
+		}
+
+		Vector operator-(Vector const& obj) {
+			xn = x + obj.x;
+			yn = y + obj.y;
+			zn = z + obj.z; 
+			return Vector(xn, yn, zn);
+		}
+};
+
 int main() {
 	cout << "Hello World";
 }
