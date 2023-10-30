0f3d1238a77a3b56aab694d26d01da81b2a866da
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-30 09:03:31 -0500

    Update spacetime.py

--- kolibri/spacetime.py
+++ kolibri/spacetime.py
@@ -6,3 +6,13 @@ from kolibri.utils import *
 from kolibri.entity import *
 from kolibri.force import *
 from kolibri.metric import *
+
+class Spacetime:
+
+	"""
+ 	As close to a god class as it gets.
+	"""
+	
+	def __init__(self, resolution: Scalar=0.000001) -> None:
+		self.resolution = resolution
+		self.calculus = Calculus(resolution)
