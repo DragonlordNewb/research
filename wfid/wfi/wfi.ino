17d823cb03e6d6989000cac1b49fc68cbfa4dedb
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-30 21:15:08 -0500

    wef

--- wfid/wfi/wfi.ino
+++ wfid/wfi/wfi.ino
@@ -6,42 +6,67 @@
 // Point of configuration: how many repetitions to run per data point emitted by the WFI.
 // Defaults to 100, so that each data point is an aggregate average of 100 individual
 // measurements.
-const char NUM_REPETITIONS = 100;
+const unsigned char MEASUREMENT_COUNT = 100;
 
-// Several basic constants to handle serial responses, delimetry, etc.
-const char COMMAND_ACCEPTED = 0;
-const char COMMAND_INVALID = 1;
+// Data array. Each int represents the time delay since the laser was activated.
+unsigned int data[MEASUREMENT_COUNT];
 
-const char 
+// Pin configurations. Can be changed as necessary.
+const char PWR_LASER = 2;    // Laser power output
+const char PWR_CONTROL = 3;  // Control photosensor power output
+const char PWR_TEST = 4;     // Test photosensor power output
+const char DATA_CONTROL = 5; // Control photosensor data input
+const char DATA_TEST = 6;    // Test photosensor data input
 
-// Data array which stores measurements before data is outputted to the master computer.
-int data[NUM_REPETITIONS];
+// Stuff for inside functions - so that memory usage is accurately calculated.
+// Add a byte for "for" loops inside the takeMeasurement function.
+char powerPin;
+unsigned long long sum = 0;
+unsigned int count = 0;
 
-void transferRawData
+double takeMeasurement(const char dataPin) {
+  if (dataPin == 5) {
+    powerPin = 3;
+  } else if (dataPin == 6) {
+    powerPin = 4;
+  } else {
+    return 0;
+  }
+
+  digitalWrite(powerPin, HIGH);
+
+  for (char i = 0; i < MEASUREMENT_COUNT; i++) {
+    count = 0;
+    digitalWrite(PWR_LASER, HIGH);
+    while (digitalRead(dataPin) == 1) {
+      count += 1;
+    }
+    digitalWrite(PWR_LASER, LOW);
+    delay(25);
+
+    data[i] = count;
+  }
+
+  digitalWrite(powerPin, LOW);
+
+  sum = 0;
+  for (char i = 0; i < MEASUREMENT_COUNT; i++) {
+    sum += data[i];
+  }
+
+  return static_cast<double>(sum) / MEASUREMENT_COUNT;
+}
 
 void setup() {
-	Serial.begin(9600);
+  Serial.begin(9600);
+
+  pinMode(PWR_LASER, OUTPUT);
+  pinMode(PWR_CONTROL, OUTPUT);
+  pinMode(PWR_TEST, OUTPUT);
+  pinMode(DATA_CONTROL, INPUT);
+  pinMode(DATA_TEST, INPUT);
 }
 
 void loop() {
-	if (Serial.available() > 0) {
-		int command = Serial.parseInt();  // Read an integer from the serial port
-
-		if (command >= 0 && command <= 255) {
-		// Here, you can perform actions based on the received command
-		// For example:
-		switch (command) {
-			case 0:
-			// Do something for command 0
-			break;
-			case 1:
-			// Do something for command 1
-			break;
-			// Add more cases for other commands as needed
-		}
-		} else {
-		// Invalid command, handle it here
-		Serial.write(INVALID_COMMAND);
-		}
-	}
+  Serial.println(takeMeasurement(5));
 }
