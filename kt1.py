9aa1ce231e5968dde31798b65ebe8f67a5d196f1
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-01 19:30:28 +0000

    aergaerg

--- kt1.py
+++ kt1.py
@@ -4,12 +4,11 @@ st = spacetime.Spacetime(0.000001)
 m = metric.Schwarzschild()
 st << m
 
-p1 = entity.Particle("p1", Vec3(0, 0, 0), 1, electric=1e-12)
+p1 = entity.Particle("p1", Vec3(0, 0, 0), 1, electric=1e-9)
 st << p1
-p1.velocity += Vec3(-1, 2, 3)
-p2 = entity.Particle("p2", Vec3(1, 1, 1), 1, electric=-1e-12)
-p2.velocity += Vec3(1, 2, 3)
+p2 = entity.Particle("p2", Vec3(1, 1, 1), 1, electric=-1e-8)
 st << p2
+p3 = entity.Particle("p3", Vec3(-1, 2, 0), 1, electric=1e-9)
 
 f = force.ElectromagneticInteraction()
 st << f
