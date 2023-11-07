f9c85024cbdba64f95845359aca6241faf75cf2f
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-07 20:07:20 +0000

    saergserg

--- kt2.py
+++ kt2.py
@@ -7,7 +7,7 @@ st << m
 p1 = entity.Particle("p1", Vec3(1, 1, 1), 1, electric=1e-6)
 p1.velocity += Vector(0, 0, 1e-3)
 st << p1
-p2 = entity.Particle("p2", Vec3(0, 0, 0), 1000000, electric=-1e-6)
+p2 = entity.Particle("p2", Vec3(0, 0, 0), 1000000, electric=-1e-4)
 st << p2
 
 f = force.ElectromagneticField()
