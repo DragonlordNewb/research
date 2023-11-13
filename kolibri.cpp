c24bcadd34733393f5130964307785c70d3a1902
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-13 20:39:16 +0000

    sterbrwtb

--- kolibri.cpp
+++ kolibri.cpp
@@ -4,11 +4,31 @@
 
 using namespace std;
 
+namespace constants {
+
+	const double c = 2.99792458e+8;
+	const double c2 = c * c;
+	const double G = 6.6743e-11;
+	const double Qe = 1.602176634e-19;
+	const double ke = 8.9875517923e+9;
+	const double alpha = 0.0072973525693;
+	const double h = 6.62607015e-34;
+	const double mu0 = 2 * (alpha / (Qe * Qe)) * h / c;
+	const double epsilon0 = 1 / (mu0 * c2);
+
+};
+
 class Vec3 {
 
 	public:
 
 		double x, y, z;
+
+		Vec3() {
+			x = 0;
+			y = 0;
+			z = 0;
+		}
 
 		Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 
@@ -93,6 +113,72 @@ class Vec4 {
 		}
 
 		// Other member functions similar to Vec3
+};
+
+class Particle {
+
+	private:
+
+		double experiencedTime;
+		Vec3 experiencedSpace;
+
+	public:
+
+		double restMass;
+		double charge;
+		Vec3 location;
+		Vec3 velocity;
+		Vec3 rotation;
+		Vec3 spin;
+
+		Particle(double restMass_=1, double charge_=1) {
+			restMass = restMass_;
+			charge = charge_;
+
+		}
+
+		double gamma() {
+			return 1 / (1 - (pow(velocity.abs(), 2) / constants::c2));
+		}
+
+};
+
+class Spacetime;
+
+class MetricComponent {
+
+	public:
+
+		MetricComponent() {}
+
+		virtual double value(Spacetime* spacetime, Vec3 location, Vec4 displacement) = 0;
+
+		double value(Vec3 location, Vec4 displacement) {
+			return 0.0;
+		}
+
+};
+
+class MetricTensor {
+
+	private:
+
+		MetricComponent* tensor[4][4];
+
+		MetricTensor() {}
+
+		MetricComponent get(int mu, int nu) {
+			return *(tensor[nu][mu])
+		}
+
+};
+
+class Spacetime {
+
+	public:
+
+		MetricTensor metric;
+
 };
 
 int main(int argc, char* argv[]) {
