9bcc9818d1e0acdada5c7d9891a52a02f283003b
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-14 20:36:56 +0000

    hj,m,fgjmghf

--- old/kolibri_o3/kt2.py
+++ old/kolibri_o3/kt2.py
@@ -8,7 +8,7 @@ p1 = entity.Particle("p1", Vec3(-1, -1, 0), 1)
 p1.velocity += Vec3(0.001, 0, 0)
 st << p1
 p2 = entity.Particle("p2", Vec3(0, 0, 0), 1e+18)
-p2.spin += Vector(0, 0, 1000000000)
+p2.spin += Vector(0, 0, -1000000000)
 st << p2
 
-st.trace("p1", 2000000)
+st.trace("p1", 2000001)
